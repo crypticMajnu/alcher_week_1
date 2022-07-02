a154b46a4156482652fafc46cf080d2e27e6509c
crypticMajnu <100520652+crypticMajnu@users.noreply.github.com>
2022-07-02 23:45:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function cb1() {
+    const elem = document.querySelector('#head1');
+    elem.innerText = "You've clicked button-1";
+}
+
+function cb2() {
+    const elem = document.querySelector('#head1');
+    elem.innerText = "You've clicked button-2";
+}
